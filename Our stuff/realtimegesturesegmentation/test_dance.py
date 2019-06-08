dd1af1a71d8dc5c6671ab81043f5bb8a8ce2e6ed
Nicole Riley <nriley16@uw.edu>
2019-06-08 13:51:06 -0700

    more changes

--- Our stuff/realtimegesturesegmentation/test_dance.py
+++ Our stuff/realtimegesturesegmentation/test_dance.py
@@ -661,7 +661,7 @@ def extract_features_from_gesture_set(gesture_set, include_custom_gesture=True,
 
             features.append(trial.trialNum)
             feature_names.append("trial_num")
-
+            
             # length
             features.append(len(trial.accel.mag))
             feature_names.append("length")
@@ -798,6 +798,7 @@ def extract_features_from_gesture_set(gesture_set, include_custom_gesture=True,
 
             list_of_feature_vectors.append(features)
             column_headers = feature_names
+            
     return (list_of_feature_vectors, column_headers)
 
 
@@ -822,6 +823,16 @@ def extract_features_from_gesture_set_hardcoded(gesture_set, include_custom_gest
             features.append(trial.trialNum)
             feature_names.append("trial_num")
 
+            features.append(trial.accel.x.mean())
+            feature_names.append("x mean")
+            features.append(np.var(trial.accel.y))
+            feature_names.append("y var")
+            features.append(trial.accel.x.max())
+            feature_names.append("x max")
+            features.append(trial.accel.mag_p.min())
+            feature_names.append("mag p min")
+
+            '''
             # length
             features.append(len(trial.accel.mag))
             feature_names.append("length")
@@ -877,8 +888,10 @@ def extract_features_from_gesture_set_hardcoded(gesture_set, include_custom_gest
             features.append(trial.accel.z.min())
             feature_names.append("z.min")       
 
+            '''
             list_of_feature_vectors.append(features)
             column_headers = feature_names
+
     return (list_of_feature_vectors, column_headers)
 
 # Feature selection using Recursive Feature Elimination (Nicole)
@@ -1076,6 +1089,90 @@ selector.fit(X_train_scaled, y_train)
 # plot_confusion_matrix(cm, classes=sorted_gesture_names, title="Scaled RFE test score: {:.3f}".format(score))
 # plt.show()
 
+def train_part_with_leave_one_out():
+    selected_gesture_set = get_gesture_set_with_str("Combined")
+    (list_of_feature_vectors, feature_names) = extract_features_from_gesture_set(selected_gesture_set,
+                                                                                include_dummy_data=True) 
+
+    df = pd.DataFrame(list_of_feature_vectors, columns = feature_names)
+    trial_indices = df.pop("trial_num")
+    X = df
+    y = df.pop('gesture')
+    gesturer = df.pop('gesturer')
+
+    # 100/0 split with stratification
+    # in this case, we have 5 gestures x 10 samples = 50 total
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, stratify=y) # random_state=42
+    # print(y_test)
+    sorted_gesture_names = sorted(y_train.unique())
+
+    # Scale values
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    clf = svm.SVC(kernel='linear')
+
+    # see: https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFE.html
+    num_features_to_eliminate = 37
+    selector = RFE(clf, n_features_to_select=len(X_train.columns) - num_features_to_eliminate)
+    selector.fit(X_train_scaled, y_train)
+
+    X_train_scaled_again = selector.transform(X_train_scaled)
+    X_test_scaled_again = selector.transform(X_test_scaled)
+
+    mask = selector.get_support()
+
+    print(len(X.columns[mask]), X.columns[mask])
+    print("eliminated vars", X.columns[~mask])
+
+    clf.fit(X_train_scaled_again, y_train)
+
+    score = clf.fit(X_train, y_train).score(X_test, y_test)
+    print("Unscaled test score: {:.3f}".format(score))
+    score = clf.fit(X_train_scaled, y_train).score(X_test_scaled, y_test)
+    print("Scaled test score: {:.3f}".format(score))
+    score = clf.fit(X_train_scaled_again, y_train).score(X_test_scaled_again, y_test)
+    print("Scaled RFE test score: {:.3f}".format(score))
+
+    y_pred = clf.fit(X_train_scaled_again, y_train).predict(X_test_scaled_again)
+    cm = confusion_matrix(y_test, y_pred, labels=sorted_gesture_names)
+    print(cm)
+    plt.figure(figsize=(10,10))
+    plot_confusion_matrix(cm, classes=sorted_gesture_names, title="Scaled RFE test score: {:.3f}".format(score))
+    plt.show()
+
+    #return (scaler, selector, clf)
+
+    # mask = selector.get_support()
+    # print(mask)
+    # print(selector.ranking_)
+    # plt.matshow(mask.reshape(1, -1), cmap='gray_r')
+    # plt.xlabel("Sample index")
+    # plt.yticks(())
+
+    # print(len(X.columns), X.columns)
+    # print(len(X.columns[mask]), X.columns[mask])
+    # print("eliminated vars", X.columns[~mask])
+
+    # X_train_scaled_rfe = selector.transform(X_train_scaled)
+    # X_test_scaled_rfe = selector.transform(X_test_scaled)
+
+    # score = clf.fit(X_train, y_train).score(X_test, y_test)
+    # print("Unscaled test score: {:.3f}".format(score))
+    # score = clf.fit(X_train_scaled, y_train).score(X_test_scaled, y_test)
+    # print("Scaled test score: {:.3f}".format(score))
+    # score = clf.fit(X_train_scaled_rfe, y_train).score(X_test_scaled_rfe, y_test)
+    # print("Scaled RFE test score: {:.3f}".format(score))
+
+    # y_pred = clf.fit(X_train_scaled_rfe, y_train).predict(X_test_scaled_rfe)
+    # cm = confusion_matrix(y_test, y_pred, labels=sorted_gesture_names)
+    # print(cm)
+    # plt.figure(figsize=(10,10))
+    # plot_confusion_matrix(cm, classes=sorted_gesture_names, title="Scaled RFE test score: {:.3f}".format(score))
+    # plt.show()
+
+
 def train_whole_recorded_gestures_elim_features():
     selected_gesture_set = get_gesture_set_with_str("Combined")
     (list_of_feature_vectors, feature_names) = extract_features_from_gesture_set(selected_gesture_set,
@@ -1101,7 +1198,7 @@ def train_whole_recorded_gestures_elim_features():
     clf = svm.SVC(kernel='linear')
 
     # see: https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFE.html
-    num_features_to_eliminate = 15
+    num_features_to_eliminate = 37
     selector = RFE(clf, n_features_to_select=len(X.columns) - num_features_to_eliminate)
     selector.fit(X_train_scaled, y)
 
@@ -1334,6 +1431,7 @@ class AccelPlot:
         # print("classify event: mag_p:")
         # print(segment_result['mag_p'])
 
+        '''
         # Selected features
         feat_length = len(mag)
         feat_x_p_mean = np.array(x_p).mean()
@@ -1350,6 +1448,11 @@ class AccelPlot:
         feat_y_max = np.array(y).max()
         feat_mag_min = np.array(mag).min()
         feat_z_min = np.array(z).min()
+        '''
+        feat_x_mean = np.array(x).mean()
+        feat_y_var = np.var(np.array(y))
+        feat_x_max = np.array(x).max()
+        feat_mag_p_min = np.array(mag_p).min()
 
         features = []
         feature_names = []
@@ -1364,6 +1467,15 @@ class AccelPlot:
         features.append("101")
         feature_names.append("trial_num")
 
+        features.append(feat_x_mean)
+        feature_names.append("x mean")
+        features.append(feat_y_var)
+        feature_names.append("y var")
+        features.append(feat_x_max)
+        feature_names.append("x max")
+        features.append(feat_mag_p_min)
+        feature_names.append("mag p min")
+        '''
         # length
         features.append(feat_length)
         feature_names.append("length")
@@ -1417,7 +1529,8 @@ class AccelPlot:
         feature_names.append("mag.min")
 
         features.append(feat_z_min)
-        feature_names.append("z.min")       
+        feature_names.append("z.min") 
+        '''      
 
         list_of_feature_vectors_event.append(features)
         column_headers = feature_names
@@ -1474,15 +1587,16 @@ class AccelPlot:
 # main() function
 def main():
     #get trained model
-    scaler, model, clf = train_whole_recorded_gestures_elim_features()
-    #scaler, model, clf = train_whole_recorded_gestures()
+    #train_part_with_leave_one_out();
+    #scaler, model, clf = train_whole_recorded_gestures_elim_features()
+    scaler, model, clf = train_whole_recorded_gestures()
 
 
     # python serial_plotter.py --port /dev/cu.usbmodem14601
 	# windows: python lserial_plotter.py --port COM5	
     # create parser
 
-    '''
+    
 
     parser = argparse.ArgumentParser(description="Accel Serial Plotter")
 
@@ -1531,7 +1645,6 @@ def main():
     accel_plot.close()
 
     print('Exiting...')
-    '''
 
 # call main
 if __name__ == '__main__':
