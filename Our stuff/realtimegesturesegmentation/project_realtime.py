4bc7ce6863cc7fcccc6b2bc3e08c5835a45d32d3
Nicole Riley <nriley16@uw.edu>
2019-06-09 16:53:25 -0700

    try again

--- Our stuff/realtimegesturesegmentation/project_realtime.py
+++ Our stuff/realtimegesturesegmentation/project_realtime.py
@@ -1206,7 +1206,7 @@ def main():
     magpLines = list()
     print("xp lines")
     for i in range(0, num_vals_xp):
-        line2dx, = ax2.plot([], [], label=labels_subplotsx[i], alpha=alphassub[i])
+        line2dx, = ax2.plot([1, 1, 2], [2, 3, 4], label=labels_subplotsx[i], alpha=alphassub[i])
         xpLines.append(line2dx)
         line2dy, = ax3.plot([], [], label=labels_subplotsy[i], alpha=alphassub[i])
         ypLines.append(line2dy)
