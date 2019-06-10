04e3f3cead4b8baf999f7d2688a41034d90c3c25
Rashmi Mudduluru <rashmi4@cs.washington.edu>
2019-06-09 18:59:17 -0700

    last

--- Our stuff/realtimegesturesegmentation/project_realtime.py
+++ Our stuff/realtimegesturesegmentation/project_realtime.py
@@ -1247,11 +1247,10 @@ def main():
     zpLines = list()
     magpLines = list()
     print("xp lines")
-    ax2.set_xlim(0, flip_rashmi_agg_x_p.shape[0])
     print(flip_rashmi_agg_x_p.shape[0])
     print(flip_rashmi_agg_x_p)
     for i in range(0, num_vals_xp):
-        line2dx, = ax2.plot(flip_rashmi_agg_x_p, label=labels_subplotsx[i], alpha=alphassub[i])
+        line2dx, = ax2.plot([],[], label=labels_subplotsx[i], alpha=alphassub[i])
         xpLines.append(line2dx)
         line2dy, = ax3.plot([], [], label=labels_subplotsy[i], alpha=alphassub[i])
         ypLines.append(line2dy)
